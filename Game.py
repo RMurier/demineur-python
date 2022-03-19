1c494063c0d2acc773d067bc41d456884e5b313e
Zyco61 <89699409+Zyco61@users.noreply.github.com>
2022-03-19 19:18:38 +0100

    update V0.5

--- Game.py
+++ Game.py
@@ -1,12 +1,19 @@
-from GUI_demineur.guiDemineur_V2 import *
 from random import randint
+import time
+
+from GUI_demineur.guiDemineur_V2 import *
+from Grille import Grille
 
 class Game(object):
 
-    def __init__(self, long):
+    def __init__(self, long=40):
+        """
+        Permet l'initialisation du jeu.
+        """
         self.gui = GUIdemineur(long, 32) #créer une instance de guiDemineur_V2
-        
-        self.gui.refresh(self.grille, )
+        self.grid = Grille(long)
+        self.flagputted = 0
+        self.gui.refresh(self.grid.grid, self.grid.nbBomb - self.flagputted, 0)
         
     def start(self):
         """
@@ -15,34 +22,52 @@ class Game(object):
             - 
         Elle permet de gérer chaque itération de la boucle, du début jusqu'à la fin (Game Over ou Win)
         """
+        coosBefore = None
+        self.gui.startTime()
         while True:
             x, y, clic = self.gui.waitClick() # renvoie les coordonnées de la case et du clic
             if clic == "D" : #si clic droit
-                pass
+                if self.grid.grid[y][x] == -1:
+                    if self.flagputted != self.grid.nbBomb:
+                        self.grid.grid[y][x] = -2
+                        self.flagputted += 1
+                elif self.grid.grid[y][x] == -2:
+                    self.flagputted -= 1
+                    self.grid.grid[y][x] = -3
+                elif self.grid.grid[y][x] == -3:
+                    self.grid.grid[y][x] = -1
+                self.gui.refresh(self.grid.grid, self.grid.nbBomb - self.flagputted, 0)
             elif clic == "G": #si clic gauche
-                pass
+                coosBefore = (x, y)
+                if self.grid.grid[y][x] == -1:
+                    pass
+                self.gui.refresh(self.grid.grid, self.grid.nbBomb - self.flagputted, 3)
+            elif clic == "R": #si clic relaché
+                print(-1)
+                if coosBefore == (x, y):
+                    print(0)
+                    if self.grid.grid[y][x] == -1:
+                        print(0)
+                        if self.grid.bombGrid[y][x] == 1:
+                            print(1)
+                            self.grid.grid[y][x] = -5
+                            self.gui.gameOver()
+                            time.sleep(1)
+                        else:
+                            print(1)
+                            self.grid.grid[y][x] = self.grid.numberNeighborBomb(x, y)
+                    self.gui.refresh(self.grid.grid, self.grid.nbBomb - self.flagputted, 0)
 
                 
 
     def propagation(self, case):
         """
-        Regarde si une case blanche est clické. Si oui, propage l'ouverture de case.
+        Regarde si une case vide est cliqué. Si oui, propage l'ouverture des cases situés à coté qui sont aussi vides.
         """
         pass
 
-class Grille(object):
 
-    def __init__ (self, long):
-        self.grid = [[-1 for x in range(long)] for y in range(long//2)]
-        self.bombGrid = [[-1 for x in range(long)] for y in range(long//2)]
-
-    def randomizeBomb(self, nbBomb):
-        #1 = bombe, sinon 0
-        for i in range(nbBomb):
-            r1 = randint(0, len(self.bombGrid) - 1)
-            r2 = randint(0, len(self.bombGrid[1]) - 1)
-            self.bombGrid[r1][r2] = 1
 
 if __name__ == '__main__' :
-    m = Game(32)
+    m = Game()
     m.start()
---
+++ Grille.py
@@ -0,0 +1,41 @@
+import random
+
+class Grille(object):
+    """
+    Initialisation de la grille, qui est utiliser pour générer une grille, ainsi qu'effectuer une modification sur cette dernière.
+    Elle prends plusieurs paramètres:
+    - La longueure n (définite sur 40 par défaut dans Game), qui définis le tableau t (t = n * n//2)
+    - Le nombre de bombe (définite sur 15 par défaut).
+        Dans le cas ou le bombre de bombes dépasse le nombre de cases disponible, le nombre de bombes sera égale a l'entier 
+        inférieur au nombre de case divisé par 8 ((longueur*largeur)//2)
+    """
+    def __init__ (self, long, nbomb = 15):
+        self.grid = [[-1 for x in range(long)] for y in range(long//2)]
+        self.bombGrid = [[0 for x in range(long)] for y in range(long//2)]
+        self.nbBomb = (long*long//2)//8 if nbomb > long*long//2 else nbomb
+        self.randomizeBomb()
+
+    def randomizeBomb(self):
+        """
+        Permet de mettre x bombes aléatoirement sur la grille lors du démarage de la partie.
+        """
+        for _ in range(self.nbBomb):
+            while True:
+                y = random.randint(0, len(self.bombGrid) - 1) #entre 0 et n-1 sur l'axe y (hauteur)
+                x = random.randint(0, len(self.bombGrid[1]) - 1) #entre 0 et n-1 sur l'axe x (largeur)
+                if self.bombGrid[y][x] == 1:
+                    continue
+                self.bombGrid[y][x] = 1
+                break
+
+    def numberNeighborBomb(self, x, y):
+        nb = 0
+        nb += 1 if x > 0 and self.bombGrid[y][x-1] == 1 else 0 #gauche
+        nb += 1 if x < len(self.bombGrid[0]) -1 and self.bombGrid[y][x+1] == 1 else 0 #droite
+        nb += 1 if y > 0 and self.bombGrid[y-1][x] == 1 else 0 #haut
+        nb += 1 if y < len(self.bombGrid) -1 and self.bombGrid[y+1][x] == 1 else 0 #bas
+        nb += 1 if (x > 0 and y > 0) and self.bombGrid[y-1][x-1] == 1 else 0 #haut gauche
+        nb += 1 if (x < len(self.bombGrid[0]) -1 and y > 0) and self.bombGrid[y-1][x+1] == 1 else 0 #haut droit
+        nb += 1 if (x > 0 and y < len(self.bombGrid) -1) and self.bombGrid[y+1][x-1] == 1 else 0 #bas gauche
+        nb += 1 if (x < len(self.bombGrid[0]) -1 and y < len(self.bombGrid) -1) and self.bombGrid[y+1][x+1] == 1 else 0
+        return nb
